0ae27514eec8f42189246e521e24b94b7fe5fbe9
angelntorron <angelntorron@gmail.com>
2016-06-26 17:54:42 +0200

    3er commit
    cambios en todas las clases, creadas algunas pruebas. No muy claro como
    seguir

--- src/principal/Prueba.java
+++ src/principal/Prueba.java
@@ -1,6 +1,48 @@
 package principal;
 
+import java.util.ArrayList;
+
 public class Prueba {
 	private int id;
+	private String descripcion;
+	static ArrayList<Prueba> pruebas=new ArrayList<Prueba>();
+
 	
+	public Prueba(int id, String descripcion){
+		this.id=id;
+		this.descripcion=descripcion;
+	}
+	public String toString() {
+		return id +" "+ descripcion;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public String escogerPrueba(){
+		return "nada";
+	}
+	
+	public void cargarPruebas(){
+	String jugBebe1="El jugador debe beber un trago";
+	Prueba jugadorBebe1=new Prueba(1, jugBebe1);
+	pruebas.add(jugadorBebe1);
+	
+	String masAlto="El jugador más alto debe beber";
+	Prueba jugadorMasAlto=new Prueba(2,masAlto);
+	pruebas.add(jugadorMasAlto);
+	
+	String hermanos="El jugador que mas hermanos/as tenga debe beber (Un trago por cada hermano/a)";
+	Prueba masHermanos=new Prueba(3, hermanos);
+	pruebas.add(masHermanos);
+	}
 }
